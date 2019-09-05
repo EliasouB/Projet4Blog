62681585bab5574995d98550cce488895c9b6184
EliasouB <elias.bouabdellah@gmail.com>
2019-09-05 15:03:09 +0200

    creation des fonctions listPosts post addComment

---
+++ controller/frontend.php
@@ -0,0 +1,44 @@
+<?php
+
+// Chargement des classes
+require_once('model/manager/PostManager.php');
+require_once('model/manager/CommentManager.php');
+require_once('model/entity/Post.php');
+
+function listPosts()
+{
+    $postManager = new PostManager(); // Création d'un objet
+    $posts = $postManager->getPosts(); // Appel d'une fonction de cet objet
+
+    require('view/frontend/listPostsView.phtml');
+}
+
+function post()
+{
+    $postManager = new PostManager();
+    $commentManager = new CommentManager();
+
+    $post = $postManager->getPost($_GET['id']);
+    $postObject = new Post();
+    $postObject->setId($post['id']);
+    $postObject->setTitle($post['title']);
+    $postObject->setContent($post['content']);
+    $postObject->setCreationDate($post['creation_date']);
+    $comments = $commentManager->getComments($_GET['id']);
+
+    require('view/frontend/postView.phtml');
+}
+
+function addComment($postId, $author, $comment)
+{
+    $commentManager = new CommentManager();
+
+    $affectedLines = $commentManager->postComment($postId, $author, $comment);
+
+    if ($affectedLines === false) {
+        throw new Exception('Impossible d\'ajouter le commentaire !');
+    }
+    else {
+        header('Location: index.php?action=post&id=' . $postId);
+    }
+}
