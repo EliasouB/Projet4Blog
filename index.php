b0e4ca5763d7663bfd132fa85768ba2919b04f20
EliasouB <elias.bouabdellah@gmail.com>
2019-09-23 12:30:15 +0200

    modification Css et index

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ try { // On essaie de faire des choses
         elseif ($_GET['action'] == 'addComment') {
             if (isset($_GET['id']) && $_GET['id'] > 0) {
                 if (!empty($_POST['author']) && !empty($_POST['comment'])) {
-                    addComment($_GET['id'], htmlspecialchars(_POST['author']), htmlspecialchars($_POST['comment']));
+                    addComment($_GET['id'], htmlspecialchars($_POST['author']), htmlspecialchars($_POST['comment']));
                 }
                 else {
                     // Autre exception
